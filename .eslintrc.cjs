5bcbcf4489806e420f55772fda8471da3181fe9e
DYLANeay <dylan.eray6@gmail.com>
2026-05-13 15:38:28 +0200

    wip: .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,6 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2022,
     sourceType: 'module',
@@ -15,17 +14,17 @@ module.exports = {
     node: true,
     es2022: true,
   },
-  plugins: ['@typescript-eslint', 'react', 'react-hooks'],
+  plugins: ['react', 'react-hooks'],
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
     'prettier',
   ],
   rules: {
     'react/react-in-jsx-scope': 'off',
-    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    'react/prop-types': 'off',
+    'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
   },
   ignorePatterns: [
     'node_modules',
@@ -35,6 +34,5 @@ module.exports = {
     'public',
     '**/*.config.js',
     '**/*.config.cjs',
-    '**/*.config.ts',
   ],
 };
