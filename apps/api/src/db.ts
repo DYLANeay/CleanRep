b7879f27d3b6c0310ac466161dfbc4a9f03b288d
DYLANeay <dylan.eray6@gmail.com>
2026-05-13 11:47:10 +0200

    chore: finalize scaffold (drop premature db.ts, refresh lockfile)

--- apps/api/src/db.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-
-let client: PrismaClient | undefined;
-
-export function db(): PrismaClient {
-  if (!client) client = new PrismaClient();
-  return client;
-}
